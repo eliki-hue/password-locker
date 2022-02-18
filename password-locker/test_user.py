ebc2b33705136a07415f2f48bf7c08cf6d16868b
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-18 20:41:23 +0300

    defined the User class

--- password-locker/test_user.py
+++ password-locker/test_user.py
@@ -6,3 +6,8 @@ class TestUser(unittest.TestCase):
     '''
     test class that defines test cases for user behaviour
     '''
+    def setUp(self) :
+        '''
+        set up method to run before each test case.
+        '''
+    self.new_user = User()
--- password-locker/user.py
+++ password-locker/user.py
@@ -3,3 +3,12 @@ class User:
     User class to perform user login operations
     '''
     pass
+    user_login_information =[]
+    
+    def __init__(self,loginName, loginPassword):
+        '''
+        __init__ method that defines the properties of user.
+        '''
+        self.loginName = loginName
+        self.loginPassword = loginPassword
+        
