9ebc218ccf1c5c4fdd7465a5ff8e8fd6ab8ebde8
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-18 20:28:55 +0300

    created test classes for both user class and credential class

---
+++ password-locker/test_credentials.py
@@ -0,0 +1,8 @@
+from credentials import Credentials
+import unittest
+
+
+class TestCredentials(unittest.TestCase):
+    '''
+    test class that defines test cases for credentials behaviour
+    '''
---
+++ password-locker/test_user.py
@@ -0,0 +1,8 @@
+from user import User
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    '''
+    test class that defines test cases for user behaviour
+    '''
