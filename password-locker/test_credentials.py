837fb2a99aa876bae88d556383693dccf6a26be3
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-18 23:42:46 +0300

    defined a test function to test saveAccount function

--- password-locker/test_credentials.py
+++ password-locker/test_credentials.py
@@ -21,6 +21,14 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_account.accountName,'facebook')
         self.assertEqual(self.new_account.userName,'eliki')
         self.assertEqual(self.new_account.password,'13720')
+
+    def test_saveAccountCredentials(self):
+        '''
+        saving credential test function
+        '''
+
+        self.new_account.saveAccount()
+        self.assertEqual(len(self.new_account),1)
         
 
 
