229a4f44cfd5eea8a89ebb9d084ca681789dde67
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-21 01:37:48 +0300

    created the account delete functionality

--- password-locker/run.py
+++ password-locker/run.py
@@ -32,7 +32,7 @@ def main():
         print('\n')
 
         while True:
-                print("Use these short codes : cc - create a new account credentials, dc - display all account credentials, fc -find a contact, ex -exit the contact list ")
+                print("Use these short codes : cc - create a new account credentials, dc - display all account credentials, fc -find a contact, del - to delete an account, ex -exit the contact list ")
 
                 short_code = input().lower()
 
@@ -53,6 +53,61 @@ def main():
                         print(f"New Account credentials {user_name} {password} for {name} account created")
                         print ('\n')
 
-               
+                elif short_code == 'dc':
+
+                        if Credentials.displayAccounts():
+                                print("Here is a list of all your accounts credentials")
+                                print('\n')
+
+                                for account in Credentials.displayAccounts():
+                                        print(f"account type: {account.accountName}     username: {account.userName} password: {account.password}")
+
+                                print('\n')
+                        else:
+                                print('\n')
+                                print("You dont seem to have any contacts saved yet")
+                                print('\n')
+
+                # elif short_code == 'fc':
+
+                #         print("Enter the number you want to search for")
+
+                #         search_number = input()
+                #         if check_existing_contacts(search_number):
+                #                 search_contact = find_contact(search_number)
+                #                 print(f"{search_contact.first_name} {search_contact.last_name}")
+                #                 print('-' * 20)
+
+                #                 print(f"Phone number.......{search_contact.phone_number}")
+                #                 print(f"Email address.......{search_contact.email}")
+                #         else:
+                #                 print("That contact does not exist")
+
+                elif short_code == 'del':
+
+                        try:
+                            
+                            print("Enter the account name you want to delete")
+                            toDelete = input("Name: ")
+
+                            for account in Credentials.accountsCredentials:
+                             if account.accountName == toDelete:
+                                 confirm_delete = confirm(f'do you really want to delete {account.accountName} username: {account.userName}  password:{account.password}')
+                                 if confirm_delete:
+                                     Credentials.deleteAccount(account)
+                                     print('\n')
+                                     print('Account deleted successfully')
+                                 else:
+                                     break
+                        except:
+                            print('The account does not exist')
+                        
+
+                elif short_code == "ex":
+                        print("Bye .......")
+                        break
+                else:
+                        print("I really didn't get that. Please use the short codes")
+
 if __name__ == '__main__': 
     main()
